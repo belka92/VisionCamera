6a26ff516fc033aa1b9dd4d11379682eedcc99fd
Bella Babayan <zbelka92@gmail.com>
2023-11-15 16:24:20 +0400

    feat: add git config for conventional commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
-        'subject-case': [2, 'always', 'sentence-case'],
-    },
+    extends: ["@commitlint/config-conventional"],
   };
