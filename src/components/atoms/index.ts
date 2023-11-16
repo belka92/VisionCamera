88ba4302e7079b37aa3764f43e8d50dff853ce10
Bella Babayan <zbelka92@gmail.com>
2023-11-16 12:58:01 +0400

    add avatar component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as TouchableButton } from './TouchableButton';
 export { default as CustomText } from './CustomText';
+export { default as Avatar } from './Avatar';
