52080ee04cb1860d1533264151d798f5308dab18
Bella Babayan <zbelka92@gmail.com>
2023-11-16 14:46:12 +0400

    feat: add Checkbox component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as TouchableButton } from './TouchableButton';
 export { default as CustomText } from './CustomText';
+export { default as Checkbox } from './Checkbox';
